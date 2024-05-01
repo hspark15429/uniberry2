5a1416e376aa48ac7384490938dc1848e3664f9f
Han Park <hspark15429@gmail.com>
2024-05-01 22:48:18 +0900

    ui course add

--- lib/core/services/router.dart
+++ lib/core/services/router.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uniberry2/core/services/injection_container.dart';
+import 'package:uniberry2/src/timetable/presentation/cubit/timetable_cubit.dart';
+import 'package:uniberry2/src/timetable/presentation/views/timetable_screen.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case '/':
-      return MaterialPageRoute(builder: (_) => const Placeholder());
+      return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+                create: (_) => sl<TimetableCubit>(),
+                child: const TimetableScreen(),
+              ));
     case '/timetable':
       return MaterialPageRoute(builder: (_) => const Placeholder());
     default:
---
+++ lib/src/timetable/presentation/views/timetable_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uniberry2/src/timetable/domain/entities/course.dart';
+import 'package:uniberry2/src/timetable/presentation/cubit/timetable_cubit.dart';
+
+class TimetableScreen extends StatefulWidget {
+  const TimetableScreen({super.key});
+
+  static const String routeName = '/timetable';
+
+  @override
+  State<TimetableScreen> createState() => _TimetableScreenState();
+}
+
+class _TimetableScreenState extends State<TimetableScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocConsumer<TimetableCubit, TimetableState>(
+        builder: (context, state) {
+          if (state is TimetableLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is CourseFetched) {
+            return Center(child: Text(state.course.toString()));
+          }
+          return Center(
+            child: TextButton(
+              onPressed: () {
+                context.read<TimetableCubit>().getCourse('MATH101');
+              },
+              child: const Text(
+                'Get Course: MATH101',
+              ),
+            ),
+          );
+        },
+        listener: (context, state) {},
+      ),
+    );
+  }
+}
