55342d624b5e84e1c1595088048b770f3979580a
Han Park <hspark15429@gmail.com>
2024-05-31 23:13:38 +0900

    injected algolia

--- lib/core/services/injection_container.dart
+++ lib/core/services/injection_container.dart
@@ -1,7 +1,9 @@
+import 'package:algolia_helper_flutter/algolia_helper_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
 import 'package:uniberry2/src/timetable/data/datasources/timetable_remote_data_source.dart';
 import 'package:uniberry2/src/timetable/data/datasources/timetable_remote_data_source_implementation.dart';
+import 'package:uniberry2/src/timetable/data/datasources/timetable_remote_data_source_implementation_algolia.dart';
 import 'package:uniberry2/src/timetable/data/repositories/timetable_repository_implementation.dart';
 import 'package:uniberry2/src/timetable/domain/repository/timetable_repository.dart';
 import 'package:uniberry2/src/timetable/domain/usecases/get_course.dart';
@@ -32,9 +34,17 @@ Future<void> initTimetable() async {
 
     // data source impl
     ..registerLazySingleton<TimetableRemoteDataSource>(
-      () => TimetableRemoteDataSourceImplementation(cloudStoreClient: sl()),
+      () =>
+          TimetableRemoteDataSourceImplementationAlgolia(coursesSearcher: sl()),
     )
 
     // external
-    ..registerLazySingleton(() => FirebaseFirestore.instance);
+    ..registerLazySingleton(() => FirebaseFirestore.instance)
+    ..registerLazySingleton(
+      () => HitsSearcher(
+        applicationID: 'K1COUI4FQ4',
+        apiKey: '00383db0c4d34b63decf046026091f32',
+        indexName: 'courses_index',
+      ),
+    );
 }
